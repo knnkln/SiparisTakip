c630df3db4bb3af18b0b35e19330ee2f57fa670c
knnkln <knnkln@hotmail.com>
2019-04-11 14:07:42 +0300

    Dal katmanı tasarlandı.

---
+++ SiparisTakip/SiparisTakip.Dal/Abstract/StokDal/IStokDal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using SiparisTakip.Entity.Models;
+
+namespace SiparisTakip.Dal.Abstract.StokDal
+{
+    public interface IStokDal
+    {
+        Stok Kaydet(Stok entity);
+        List<Stok> Listele();
+        List<Stok> Listele(Expression<Func<Stok, bool>> predicateExpression);
+        Stok Getir(int id);
+        int Guncelle(Stok entity);
+        bool Sil(int id);
+        bool Sil(Stok entity);
+    }
+}
---
+++ SiparisTakip/SiparisTakip.Dal/Concrete/EntityFramework/Repository/EFStokRepository.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using SiparisTakip.Dal.Abstract.StokDal;
+using SiparisTakip.Dal.Concrete.EntityFramework.Context;
+using SiparisTakip.Entity.Models;
+
+namespace SiparisTakip.Dal.Concrete.EntityFramework.Repository
+{
+    class EFStokRepository : IStokDal
+    {
+
+
+
+
+        public Stok Kaydet(Stok entity)
+        {
+            using (SiparisTakipContext context = new SiparisTakipContext())
+            {
+                context.Stok.Add(entity);
+                context.SaveChanges();
+                return entity;
+            }
+        }
+
+        public List<Stok> Listele()
+        {
+            using (SiparisTakipContext context = new SiparisTakipContext())
+            {
+                return context.Stok.AsNoTracking().ToList();
+            }
+        }
+
+        public List<Stok> Listele(Expression<Func<Stok, bool>> predicateExpression)
+        {
+            using (SiparisTakipContext context = new SiparisTakipContext())
+            {
+                return context.Stok.AsNoTracking().Where(predicateExpression).ToList();
+            }
+        }
+
+        public Stok Getir(int id)
+        {
+            using (SiparisTakipContext context = new SiparisTakipContext())
+            {
+                return context.Stok.AsNoTracking().SingleOrDefault(x => x.StokID == id);
+            }
+        }
+
+        public int Guncelle(Stok entity)
+        {
+            using (SiparisTakipContext context = new SiparisTakipContext())
+            {
+                context.Stok.AddOrUpdate(entity);
+                return context.SaveChanges();
+            }
+        }
+
+        public bool Sil(int id)
+        {
+            using (SiparisTakipContext context = new SiparisTakipContext())
+            {
+                Stok silinecek = Getir(id);
+                return Sil(silinecek);
+            }
+        }
+
+        public bool Sil(Stok entity)
+        {
+            using (SiparisTakipContext context = new SiparisTakipContext())
+            {
+                context.Stok.Remove(entity);
+                return context.SaveChanges() > 0;
+            }
+        }
+    }
+}
